afd129fa5c234f9fbcaa9b62321bc861493d0e1b
arjun singh <arjunics027@gmail.com>
2020-02-04 22:28:48 +0530

    Initial commit

---
+++ src/actions/searchActions.js
@@ -0,0 +1,21 @@
+import { SEARCH_MOVIE, FETCH_MOVIES } from './types';
+import axios from 'axios';
+import { ApiKey } from '../ApiKey';
+
+export const searchMovie = text => dispatch => {
+   dispatch({
+      type: SEARCH_MOVIE,
+      payload: text
+   })
+};
+
+export const fetchMovies = text => dispatch => {
+   axios.get(`http://www.omdbapi.com/?apikey=${ApiKey}&s=${text}`)
+      .then(response => {
+         dispatch({
+            type: FETCH_MOVIES,
+            payload: response.data
+         })
+      })
+      .catch(err => console.log)
+};
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Spinner() {
+   return (
+      <div>
+         <img src="fas fa-circle-notch" style={{ width: '200px', margin: 'auto', display: "block" }} alt="Loading..." />
+      </div>
+   )
+}
+
+export default Spinner
